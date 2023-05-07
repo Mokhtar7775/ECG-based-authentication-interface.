dd7a6f1697e15c4c171e7796b65aec2e98b9dfed
Mokhtar Emad <mokhtaremad5@gmail.com>
2023-05-08 01:09:28 +0300

    final

--- main.py
+++ main.py
@@ -1,48 +1,93 @@
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
-import glob
-def moving_window_integration(signal, window_size, overlap):
-    window = np.hanning(window_size)
-    step = window_size - overlap
-    result = np.zeros_like(signal)
-    for i in range(0, len(signal) - window_size, step):
-        result[i:i+window_size] += window * signal[i:i+window_size]
-    return result
+import wfdb as wfdb
 
-dataset = glob.glob('DataSet/*')
+
+def apply_window(ecg_signal,size):
+    window_size =size
+    window = np.ones(window_size) / window_size
+    smooth_ecg_signal = np.convolve(ecg_signal, window, mode='same')
+    return smooth_ecg_signal
+
+
+def thresholding(signal):
+    factor = 0.7
+    threshold = factor*np.std(signal)
+    r_peaks = []
+    check = False
+    tmp = []
+    indices = []
+    for i in range(0, len(signal)):
+        if signal[i] > threshold:
+            check = True
+            tmp.append(i)
+        else:
+            if check:
+                indices.append(tmp)
+                tmp = []
+            check = False
+    for i in indices:
+        indx = i[int(len(i)/2)]
+        # avg = int(np.sum(i) / len(i))
+        r_peaks.append(indx)
+        signal[indx] = 1
+    for i in range(0, len(signal)):
+        if signal[i] != 1:
+            signal[i] = 0
+    return signal,r_peaks
+
+def qr_peak():
+    x= []
+    for i in range(len(r_peaks)):
+        print(i)
+        if i !=0:
+            x.append(np.argmin(filtered[r_peaks[i-1]:r_peaks[i]]))
+    return x
+
+
+dataset = os.listdir('DataSet')
 for patient in dataset:
-    records = glob.glob(patient + '\\*')
+    records = os.listdir('DataSet\\'+patient)
     for record in records:
-        file = np.fromfile(record, dtype='byte')
+        a = wfdb.rdsamp('Dataset'+'/'+patient+'/'+record+"/"+record)
+        b = np.asarray(a[0])
+        b = b.reshape(15,115200)
+        file = b[8]
         nyq = 0.5 * 1000
-        low = 1.0 / nyq
-        high = 40.0 / nyq
-        num, den = signal.butter(2, [low, high], btype='bandpass', output='ba')
-        plt.plot(np.arange(0, 200), file[slice(200)])
-        plt.title(" ECG Signal of " + record[11:])
-        plt.show()
-        amp = list(file[0:10000])
-        filtered = signal.lfilter(num, den, amp)
-        plt.plot(np.arange(0, 200), filtered[slice(200)])
-        plt.title("Filtered Signal of " + record[11:])
-        plt.show()
-        diff = np.diff(filtered, n=1)
-        plt.plot(np.arange(0, 200), diff[slice(200)])
-        plt.title("Derevatived Signal of " + record[11:])
-        plt.show()
-        sq = diff ** 2
-        plt.plot(np.arange(0, 200), sq[slice(200)])
-        plt.title("Squaring Signal of " + record[11:])
-        plt.show()
-        win = moving_window_integration(sq,100,50)
-        print(len(win))
-        plt.plot(np.arange(0, 200), win[slice(200)])
-        plt.title("Windowed Signal of " + record[11:])
-        plt.show()
-        threshold = 0.7* np.max(win)  # Threshold for QRS detection
-        r_signal = np.zeros(win.shape)
-        r_signal[win > threshold] = 1
-        plt.plot(np.arange(0, 200), r_signal[slice(200)])
-        plt.title("Thresholding Signal of " + record[11:])
+        low = 10.0/ nyq
+        high = 40.0/ nyq
+        file = signal.resample(file,11520)
+        fc_notch = 50  # Notch frequency (Hz)
+        Q = 30
+        w0 = fc_notch / (1000 / 2)
+        bb, aa = signal.iirnotch(w0, Q)
+        ecg_data = signal.filtfilt(bb, aa, file)
+        b_s, a_s = signal.butter(2, [low,high], btype='bandpass')
+        filtered = signal.filtfilt(b_s, a_s, ecg_data)
+        diff = np.gradient(filtered,edge_order=2)
+        squared = diff*diff
+        win = apply_window(squared, int(0.2*1000))
+        win1 = win.copy()
+        thresholded,r_peaks = thresholding(win1)
+        print(r_peaks)
+
+
+
+        fig, axs = plt.subplots(3, 2, figsize=(8, 6))
+        axs[0][0].plot(np.arange(0, len(file)), file)
+        axs[0][0].set_title(patient+'--'+record)
+        axs[0][1].scatter(r_peaks, filtered[r_peaks], marker='x', color='red')
+        axs[0][1].plot(np.arange(0, len(filtered)), filtered)
+        axs[0][1].set_title('filtered Signal')
+        axs[1][0].plot(np.arange(0, len(diff)), diff)
+        axs[1][0].set_title('diff signal')
+        axs[1][1].plot(np.arange(0,len(squared)), squared)
+        axs[1][1].set_title('squared signal')
+        axs[2][0].plot(np.arange(0, len(win)), win)
+        axs[2][0].set_title('windowed signal')
+        axs[2][1].plot(np.arange(0, len(thresholded)), thresholded)
+        axs[2][1].set_title('thresholded')
+        plt.tight_layout()
         plt.show()
